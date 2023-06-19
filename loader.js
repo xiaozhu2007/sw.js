2f8b63d63c73d5668b951190b3fce0c0aa785a03
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2023-06-19 21:47:51 +0800

    Halo World!

---
+++ loader.js
@@ -0,0 +1,34 @@
+const getFileBlob = async (url) => {
+  const FileResponse = await fetch(url);
+  if (!FileResponse.ok) {
+    throw new Error(
+      `File didn't load successfully; error code: ${
+        FileResponse.statusText || FileResponse.status
+      }`
+    );
+  }
+  return FileResponse.blob();
+};
+
+const registerServiceWorker = async (url) => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register(url, {
+        scope: "/",
+      });
+      if (registration.installing) {
+        console.log("正在安装 Service worker");
+      } else if (registration.waiting) {
+        console.log("已安装 Service worker installed");
+      } else if (registration.active) {
+        console.log("激活 Service worker");
+      }
+    } catch (error) {
+      console.error(`注册失败：${error}`);
+    }
+  }
+};
+
+registerServiceWorker(
+  getFileBlob(`https://xiaozhu2007.github.io/sw.js/index.js?_t=${Date.now()}`)
+);
